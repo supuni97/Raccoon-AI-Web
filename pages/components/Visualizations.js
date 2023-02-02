7b397ed0e269d523676f9dce1307e0904143395e
supuni <sitharabandara1997@gmail.com>
2023-02-02 11:24:30 +0530

    Added files

---
+++ pages/components/Visualizations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+export default function Visualizations() {
+  return (
+    <>
+    <Box height={100}/>
+    <div>Visualizations</div>
+    </>
+    
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Sidebar from './Sidebar'
+
+export default function Home() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
